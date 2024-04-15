ebaec552a213d8efd992dceae8159c394b3ef523
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-04-15 15:19:57 +0900

    프로젝트 생성

---
+++ WishListApp/WishListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WishListApp
+//
+//  Created by 배지해 on 4/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
